b6bee6c55a113ec42f851c5696b4d05ef46433bf
frytime32 <frytime32@gmail.com>
2018-04-24 12:00:02 -0600

    Updated supported file types

---
+++ FileTypeEnum.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class FileTypeEnum(Enum):
+	CSV = 0
+	JSON = 1
+	HTML = 2
+	Clipboard =3 
+	Excel = 4
+	HDF5 =5
+	Feather = 6
+	Parquet = 7
+	MsgPack = 8
+	Stata = 9
+	Pickle = 10
+	SQL = 11
+	GBQ = 12
+
