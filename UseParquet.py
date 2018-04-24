27e099b24c649b5ec825327c942d9bd508abbf9b
frytime32 <frytime32@gmail.com>
2018-04-24 11:59:21 -0600

    Export function in place and working

--- UseParquet.py
+++ UseParquet.py
@@ -1,10 +1,13 @@
 import pyarrow.parquet as pq
 import pyarrow as pa
 import pandas as pd
+import xlsxwriter
+from sqlalchemy import create_engine
 from ColumnInfo import ColumnInfo
 from ContinuousQuery import ContinuousQuery
 from DiscreteQuery import DiscreteQuery
 from OperatorEnum import OperatorEnum
+from FileTypeEnum import FileTypeEnum
 import sys
 
 def peek(parquetFilePath, numRows=10, numCols=10)->pd.DataFrame:
@@ -40,19 +43,30 @@ def getColumnNames(parquetFilePath)->list:
 	
 	p = pq.ParquetFile(parquetFilePath)
 	columnNames = p.schema.names
+	#delete 'Sample' from schema
 	del columnNames[0]
-	del columnNames[len(columnNames)-1]
+
+	#delete extraneous other schema that the parquet file tacks on at the end
+	if '__index_level_' in columnNames[len(columnNames)-1]:
+		del columnNames[len(columnNames)-1]
+	if 'Unnamed:' in columnNames[len(columnNames)-1]:
+		del columnNames[len(columnNames)-1]
 	return columnNames
 
-def getColumnInfo(parquetFilePath, columnName:str)->ColumnInfo:
+def getColumnInfo(parquetFilePath, columnName:str, sizeLimit:int=None)->ColumnInfo:
 	"""
 	Given a parquet file and column name, returns a ColumnInfo object describing the column's name,data type (discrete/continuous), and all its unique values	
 	"""
 	columnList = [columnName]
 	df = pd.read_parquet(parquetFilePath, columns=columnList)
 	uniqueValues = set()
+	counter =0
 	for index, row in df.iterrows():
 		uniqueValues.add(row[columnName])
+		counter+=1
+		if sizeLimit != None:
+			if counter>=sizeLimit:
+				break
 	uniqueValues = list(uniqueValues)
 	if isinstance(uniqueValues[0],str):
 		return ColumnInfo(columnName,"discrete", uniqueValues)
@@ -72,7 +86,8 @@ def query(parquetFilePath, columnList: list=[], continuousQueries: list=[], disc
 	for query in continuousQueries:
 		columnList.append(query.columnName)
 	for query in discreteQueries:
-		columnList.append(query.columnName)
+		if query.columnName not in columnList:
+			columnList.append(query.columnName)
 	columnList.insert(0,"Sample")
 	df = pd.read_parquet(parquetFilePath, columns = columnList)
 	df.set_index("Sample", drop=True, inplace=True)
@@ -92,10 +107,33 @@ def query(parquetFilePath, columnList: list=[], continuousQueries: list=[], disc
 			df = df.loc[df[query.columnName]<=query.value, [ col for col in columnList]]
 	#perform discrete queries
 	for query in discreteQueries:
-		df = df.loc[df[query.columnName] == query.value, [col for col in columnList]]
+		df = df.loc[df[query.columnName].isin(query.values), [col for col in columnList]]
 	
 	return df
 
-
-
+def exportQueryResults(parquetFilePath, outFilePath, outFileType:FileTypeEnum, columnList: list=[], continuousQueries: list=[], discreteQueries: list=[]):
+	"""Wrapper function for query that exectues queries then exports them to the given file type, such as JSON or CSV
+	"""
+	df = query(parquetFilePath, columnList, continuousQueries, discreteQueries)
+	if outFileType== FileTypeEnum.CSV:
+		df.to_csv(path_or_buf=outFilePath, sep='\t')
+	elif outFileType == FileTypeEnum.JSON:
+		df.to_json(path_or_buf=outFilePath)
+	elif outFileType == FileTypeEnum.Excel:
+		writer = pd.ExcelWriter(outFilePath, engine='xlsxwriter')
+		df.to_excel(writer, sheet_name='Sheet1') 
+		writer.save()
+	elif outFileType == FileTypeEnum.Feather:
+		df=df.reset_index()
+		df.to_feather(outFilePath)
+	elif outFileType ==FileTypeEnum.HDF5:
+		df.to_hdf(outFilePath, "group", mode= 'w')
+	elif outFileType ==FileTypeEnum.MsgPack:
+		df.to_msgpack(outFilePath)
+	elif outFileType ==FileTypeEnum.Parquet:
+		df.to_parquet(outFilePath)
+	elif outFileType == FileTypeEnum.Stata:
+		df.to_stata(outFilePath)
+	elif outFileType == FileTypeEnum.Pickle:
+		df.to_pickle(outFilePath)
 
